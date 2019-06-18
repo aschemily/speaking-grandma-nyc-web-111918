ab0b473e326e0e8338ac7b32bb7aadbe2ac5048d
Emily Asch <e.asch@me.com>
2019-06-18 23:10:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,8 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase.uppercase 
     return "NO, NOT SINCE 1938!"
-  else phrase === "I"
+  else phrase === "I LOVE YOU GRANDMA!"
+    return 
 end
 
 # Whatever you say to grandma, she should respond with
